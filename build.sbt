3dddb1b410f8867b2d186d30e6373b0a936b2981
Kouhei Motoya <kouhei.motoya@gmail.com>
2014-07-31 15:58:30 +0900

    initial project version

---
+++ build.sbt
@@ -0,0 +1,5 @@
+resolvers += "Jzy3d Snapshots" at "http://www.jzy3d.org/maven/releases"
+
+libraryDependencies += "org.jzy3d" % "jzy3d-api" % "0.9.1"
+
+scalaVersion := "2.11.1"
---
+++ src/main/scala/SurfaceDemo.scala
@@ -0,0 +1,47 @@
+package org.jzy3d.demos.surface
+
+import org.jzy3d.analysis.AbstractAnalysis
+import org.jzy3d.analysis.AnalysisLauncher
+import org.jzy3d.chart.factories.AWTChartComponentFactory
+import org.jzy3d.colors.Color
+import org.jzy3d.colors.ColorMapper
+import org.jzy3d.colors.colormaps.ColorMapRainbow
+import org.jzy3d.maths.Range
+import org.jzy3d.plot3d.builder.Builder
+import org.jzy3d.plot3d.builder.Mapper
+import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid
+import org.jzy3d.plot3d.primitives.Shape
+import org.jzy3d.plot3d.rendering.canvas.Quality
+
+import scala.math._
+
+class SurfaceDemo extends AbstractAnalysis{
+
+  override def init: Unit = {
+    var mapper: Mapper = new Mapper() {
+      def f(x: Double, y: Double) = {
+				val xDash = x * cos(Pi / 4) - y * sin(Pi / 4)
+				val yDash = x * sin(Pi / 4) + y * cos(Pi / 4)
+			3 * exp(-1 * (pow(xDash / 1, 2) + pow(yDash / 0.5, 2))) * cos(-2 * Pi * (1 * xDash + 0.5 * yDash) - 0.0) 
+      }
+    }
+
+    val range: Range = new Range(-3, 3)
+    val steps: Int = 80
+
+    val surface: Shape = Builder.buildOrthonormal(new OrthonormalGrid(range, steps, range, steps), mapper)
+    surface.setColorMapper(new ColorMapper(new ColorMapRainbow(), -surface.getBounds().getZmax(), surface.getBounds().getZmax(), new Color(1, 1, 1, .5f)))
+    surface.setFaceDisplayed(true);
+    surface.setWireframeDisplayed(false);
+
+    // Create a chart
+    chart = AWTChartComponentFactory.chart(Quality.Advanced, getCanvasType())
+    chart.getScene().getGraph().add(surface)
+	}
+}
+
+object SurfaceDemo{
+  def main(args: Array[String]) = {
+    AnalysisLauncher.open(new SurfaceDemo())
+  }
+}
